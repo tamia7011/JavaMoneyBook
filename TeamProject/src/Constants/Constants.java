c073cfe3b77532ce26aec10aa48ac731b3cb361b
jintae <606jintae@naver.com>
2017-10-14 14:27:06 +0900

    Revert "added output function of table"
    This reverts commit e6080e3b0c9e1749b668bb08f30b97f51c7d19ec.

--- TeamProject/src/Constants/Constants.java
+++ TeamProject/src/Constants/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
 	public static final int CAL_HEIGHT = 6;
 	// Fixed = expenses you can't control,  Flexible = necessary for day-to-living but you can control the amount, 
 	// Discretionary = not essential for day-to-living and you can control the amount
-	//public static enum expenseType {Fixed, Flexible, Discretionary};
+	public static enum expenseType {Fixed, Flexible, Discretionary};
 	
 }
